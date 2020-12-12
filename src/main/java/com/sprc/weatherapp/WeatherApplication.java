a7f4f243cf5e478784a5c24c5019ec9207b8e093
gabivoiq <gabriel.voicu1998@gmail.com>
2020-12-12 02:17:00 +0200

    more cleanup

--- src/main/java/com/sprc/weatherapp/WeatherApplication.java
+++ src/main/java/com/sprc/weatherapp/WeatherApplication.java
@@ -1,24 +1,10 @@
 package com.sprc.weatherapp;
 
-import com.sprc.weatherapp.api.city.CityRepository;
-import com.sprc.weatherapp.api.country.CountryRepository;
-import com.sprc.weatherapp.api.temperature.TemperatureRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WeatherApplication {
-
-    @Autowired
-    private CityRepository cityRepository;
-
-    @Autowired
-    private TemperatureRepository temperatureRepository;
-
-    @Autowired
-    private CountryRepository countryRepository;
-
     public static void main(String[] args) {
         SpringApplication.run(WeatherApplication.class, args);
     }
